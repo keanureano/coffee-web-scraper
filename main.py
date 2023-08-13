1f2094f1ca454072229395e7bc76c369231ae64b
Keanu Reaño <reanokeanu@gmail.com>
2023-08-13 09:13:54 +0800

    [gac] feat: add search functionality Added search functionality to the main script using Selenium and Chrome WebDriver. This allows the user to click on the search icon, triggering a search action on the website.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from undetected_chromedriver import Chrome
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 WEBSITE_URL = os.getenv("WEBSITE_URL")
@@ -9,6 +10,9 @@ WEBSITE_URL = os.getenv("WEBSITE_URL")
 def main():
     driver = Chrome()
     driver.get(WEBSITE_URL)
+
+    search_icon = driver.find_element(By.CLASS_NAME, "desktop-search-icon")
+    search_icon.click()
 
     input("Press Enter to Close")
     driver.close()
