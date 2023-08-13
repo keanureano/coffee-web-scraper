7387ebdd49a3d05aa35d321ee84e3baa02ebe7cd
Keanu Reaño <reanokeanu@gmail.com>
2023-08-13 18:29:29 +0800

    [gac] feat: add support for saving product data to CSV This commit adds functionality to save product data to a CSV file. It includes modifications to the .gitignore file to exclude CSV files and adds code to the main.py file. The main() function now initializes a Chrome driver, retrieves product titles from environment variables, performs web searches, extracts product descriptions, and then writes the data to an output CSV file named "output.csv".

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 from dotenv import load_dotenv
 import os
 from undetected_chromedriver import Chrome
@@ -10,14 +11,12 @@ PRODUCTS_CSV = os.getenv("PRODUCTS_CSV")
 
 def main():
     driver = Chrome()
-    product_titles = [
-        product.strip().replace(" ", "+") for product in PRODUCTS_CSV.split(",")
-    ]
+    product_titles = [product.strip() for product in PRODUCTS_CSV.split(",")]
 
     product_data = []
 
     for product_title in product_titles:
-        driver.get(f"{SEARCH_URL}?search={product_title}")
+        driver.get(f"{SEARCH_URL}?search={product_title.replace(' ', '+')}")
 
         product_result = driver.find_element(By.CSS_SELECTOR, "li.item.result")
         product_result.click()
@@ -27,7 +26,11 @@ def main():
         ).text
         product_data.append((product_title, product_description))
 
-    print(product_data)
+    with open("output.csv", "w", newline="", encoding="utf-8") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["product_title", "product_description"])
+        csv_writer.writerows(product_data)
+
     driver.close()
 
 
