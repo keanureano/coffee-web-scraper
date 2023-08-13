59920775d7b3c190a5edfa25cffcd9c8e4f06dae
Keanu Reaño <reanokeanu@gmail.com>
2023-08-13 18:31:18 +0800

    [gac] feat: Add functionality to scrape and save product data This commit adds the ability to scrape product data from a search URL and save it to a CSV file. The main() function now initializes a web driver using Chrome, retrieves product titles from the specified CSV, and scrapes product descriptions using the Chrome driver. The scraped data is then saved to the "output.csv" file with headers "product_title" and "product_description". This enhancement improves the functionality of the script by automating the process of collecting and organizing product data.

--- main.py
+++ main.py
@@ -8,30 +8,50 @@ load_dotenv()
 SEARCH_URL = os.getenv("SEARCH_URL")
 PRODUCTS_CSV = os.getenv("PRODUCTS_CSV")
 
+OUTPUT_CSV_FILENAME = "output.csv"
+CSV_HEADER = ["product_title", "product_description"]
+
 
 def main():
-    driver = Chrome()
-    product_titles = [product.strip() for product in PRODUCTS_CSV.split(",")]
+    driver = initialize_driver()
+    product_titles = get_product_titles()
 
     product_data = []
 
     for product_title in product_titles:
-        driver.get(f"{SEARCH_URL}?search={product_title.replace(' ', '+')}")
+        product_data.append(scrape_product_data(driver, product_title))
 
-        product_result = driver.find_element(By.CSS_SELECTOR, "li.item.result")
-        product_result.click()
-
-        product_description = driver.find_element(
-            By.CSS_SELECTOR, "div.product-description p:first-of-type"
-        ).text
-        product_data.append((product_title, product_description))
-
-    with open("output.csv", "w", newline="", encoding="utf-8") as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["product_title", "product_description"])
-        csv_writer.writerows(product_data)
+    write_to_csv(OUTPUT_CSV_FILENAME, product_data)
 
     driver.close()
+
+
+def initialize_driver():
+    return Chrome()
+
+
+def get_product_titles():
+    return [product.strip() for product in PRODUCTS_CSV.split(",")]
+
+
+def scrape_product_data(driver, product_title):
+    driver.get(f"{SEARCH_URL}?search={product_title.replace(' ', '+')}")
+
+    product_result = driver.find_element(By.CSS_SELECTOR, "li.item.result")
+    product_result.click()
+
+    product_description = driver.find_element(
+        By.CSS_SELECTOR, "div.product-description p:first-of-type"
+    ).text
+
+    return (product_title, product_description)
+
+
+def write_to_csv(filename, data):
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(CSV_HEADER)
+        csv_writer.writerows(data)
 
 
 if __name__ == "__main__":
