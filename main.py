1fe358d1f3acc39f85342a40915c4d991e745df6
Keanu Reaño <reanokeanu@gmail.com>
2023-08-13 09:08:53 +0800

    [gac] feat: add initial script to load website using undetected Chrome driver Added an initial script that utilizes the undetected Chrome driver to load a website. The script loads environment variables from .env using python-dotenv and accesses the website URL specified. After loading the page, the script prompts the user to press Enter to close the browser window. Files changed: Added .env.example (new file) Added .gitignore (new file) Modified main.py Modified requirements.txt Note: The commit message is based on the provided Git diff log and is generated in accordance with conventional commit message guidelines.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from dotenv import load_dotenv
+import os
+from undetected_chromedriver import Chrome
+
+load_dotenv()
+WEBSITE_URL = os.getenv("WEBSITE_URL")
+
+
 def main():
-    pass
+    driver = Chrome()
+    driver.get(WEBSITE_URL)
+
+    input("Press Enter to Close")
+    driver.close()
 
 
 if __name__ == "__main__":
